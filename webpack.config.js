102a17780fae9a79d89cca1b2ceac7023a3511cd
Arslan Goraya <gorayaa66@gmail.com>
2022-01-17 11:05:20 +0500

    wip: final

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     devServer: {
-        contentBase: path.join(__dirname, 'public/js'),
+        contentBase: path.join(__dirname, 'public'),
         port: 8080
     },
     entry: [
